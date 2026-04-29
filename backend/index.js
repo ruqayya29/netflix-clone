a765b98d11f71b4dc4acc277fdbc8c66d051d704
ruqayya29 <luhayalbeevi@gmail.com>
2026-04-29 15:09:05 +0530

    fix cors

--- backend/index.js
+++ backend/index.js
@@ -1,43 +1,46 @@
-const express=require("express")
-const cors=require("cors")
+const express = require("express")
+const cors = require("cors")
 
-const app=express()
+const app = express()
 
-app.use(cors({origin:"*"}))
+app.use(cors({
+  origin: "https://netflix-clone-g12ar4bai-ruqayya29s-projects.vercel.app",
+  methods: ["GET", "POST"],
+  credentials: true
+}))
 
 app.use(express.json())
 
-const loginuser="netflix@gmail.com"
-const loginpass= "netflix#963"
+const loginuser = "netflix@gmail.com"
+const loginpass = "netflix#963"
 
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
 
-app.post("/login",function(req,res){
-  try{
+app.post("/login", function (req, res) {
+  try {
     console.log("Body received:", req.body)
-  
 
-const { email, password } = req.body
 
-  if(loginuser===req.body.email && loginpass===req.body.password)
-  {
-    res.send(true)
+    const { email, password } = req.body
+
+    if (loginuser === req.body.email && loginpass === req.body.password) {
+      res.send(true)
+    }
+
+    else {
+      res.send(false)
+    }
+
+  } catch (err) {
+    console.error("Login error:", err)
+    res.status(500).send("Internal Server Error")
+
   }
-
-  else{
-    res.send(false)
-  }
-
-} catch(err){
-   console.error("Login error:", err) 
-   res.status(500).send("Internal Server Error")
-
-}
 })
 
 
- app.listen(3000,function(){
+app.listen(3000, function () {
   console.log("Server Started")
 })
 
-module.exports=app
+module.exports = app
